d23fd04f79d2dc3630274ee4ce0b0ff5288f6733
Erling Hauan <erling.hauan@digdir.no>
2025-01-26 11:24:54 +0100

    Add MemberButton component

---
+++ src/MainContent/MemberList/MemberButton/MemberButton.tsx
@@ -0,0 +1,18 @@
+﻿import React, { ReactElement } from "react";
+import classes from "./MemberButton.module.css";
+
+type MemberButtonProps = {
+  name: string;
+  onClick: (name: string) => void;
+};
+
+export const MemberButton = ({
+  name,
+  onClick: handleClick,
+}: MemberButtonProps): ReactElement => {
+  return (
+    <button className={classes.memberButton} onClick={() => handleClick(name)}>
+      {name}
+    </button>
+  );
+};
