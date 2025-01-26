669e3dc484e976e289649cc0cc64119b22512f66
Monticulos <131489541+Monticulos@users.noreply.github.com>
2025-01-26 10:30:19 +0100

    Test header and footer (#28)
    * Add vitest workflow
    
    * use node 22
    
    * update yaml headings
    
    * update yaml headings
    
    * Create MemberList component
    
    * Add Calendar component
    
    * test header and footer
    
    ---------
    
    Co-authored-by: Erling Hauan <erling.hauan@digdir.no>

---
+++ src/Footer/Footer.test.tsx
@@ -0,0 +1,11 @@
+﻿import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Footer } from "./Footer.tsx";
+
+describe("Footer", () => {
+  it("renders footer text", () => {
+    render(<Footer />);
+    const footerText = screen.getByText("Band practice planner");
+    expect(footerText).toBeInTheDocument();
+  });
+});
