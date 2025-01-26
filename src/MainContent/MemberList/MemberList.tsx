3c95cf3ffff4c5db3787cac11aa83df86ba0dbb9
Erling Hauan <erling.hauan@digdir.no>
2025-01-26 10:05:07 +0100

    Create MemberList component

---
+++ src/MainContent/MemberList/MemberList.tsx
@@ -0,0 +1,10 @@
+﻿import React, { ReactElement } from "react";
+import classes from "./MemberList.module.css";
+
+export const MemberList = (): ReactElement => {
+  return (
+    <div className={classes.container}>
+      <p>John Doe</p>
+    </div>
+  );
+};
