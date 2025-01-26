383fc57a30b9880fe8689ba77358d118e7650cd5
Erling Hauan <erling.hauan@digdir.no>
2025-01-26 10:29:43 +0100

    test header and footer

---
+++ src/Header/Header.test.tsx
@@ -0,0 +1,11 @@
+﻿import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Header } from "./Header.tsx";
+
+describe("Header", () => {
+  it("renders heading", () => {
+    render(<Header />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+});
