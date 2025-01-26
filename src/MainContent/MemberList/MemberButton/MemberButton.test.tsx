73a9263ba07734232d5409c1626d1002def6ea39
Monticulos <131489541+Monticulos@users.noreply.github.com>
2025-01-26 11:27:53 +0100

    Add MemberButton component (#29)
    Co-authored-by: Erling Hauan <erling.hauan@digdir.no>

---
+++ src/MainContent/MemberList/MemberButton/MemberButton.test.tsx
@@ -0,0 +1,26 @@
+﻿import React from "react";
+import { MemberButton } from "./MemberButton.tsx";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
+
+const defaultProps = { name: "Ola Nordstoga", onClick: vi.fn() };
+
+describe("MemberButton", () => {
+  it("should display name of band member", () => {
+    render(<MemberButton {...defaultProps} />);
+    const button = screen.getByRole("button", { name: defaultProps.name });
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should call click handler when clicked", async () => {
+    const user = userEvent.setup();
+    const handleClickMock = vi.fn();
+    render(<MemberButton {...defaultProps} onClick={handleClickMock} />);
+
+    const button = screen.getByRole("button", { name: defaultProps.name });
+    await user.click(button);
+
+    expect(handleClickMock).toHaveBeenCalledTimes(1);
+  });
+});
--- src/MainContent/MemberList/MemberList.tsx
+++ src/MainContent/MemberList/MemberList.tsx
@@ -1,10 +1,17 @@
 ﻿import React, { ReactElement } from "react";
 import classes from "./MemberList.module.css";
+import { MemberButton } from "./MemberButton/MemberButton.tsx";
 
 export const MemberList = (): ReactElement => {
+  const handleClickMember = (name: string): void => {
+    alert(`${name} was clicked`);
+  };
+
   return (
     <div className={classes.container}>
-      <p>John Doe</p>
+      <MemberButton name={"John Doe"} onClick={handleClickMember} />
+      <MemberButton name={"Sallie Ann"} onClick={handleClickMember} />
+      <MemberButton name={"Ola Nordmann"} onClick={handleClickMember} />
     </div>
   );
 };
