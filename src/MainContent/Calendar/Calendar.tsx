74bd18de51847da572a57eb0cf627fb026ad7052
Monticulos <131489541+Monticulos@users.noreply.github.com>
2025-01-26 15:46:20 +0100

    Create calendar base (#33)
    Co-authored-by: Erling Hauan <erling.hauan@digdir.no>

--- src/MainContent/Calendar/Calendar.tsx
+++ src/MainContent/Calendar/Calendar.tsx
@@ -1,10 +1,21 @@
 ﻿import React, { ReactElement } from "react";
 import classes from "./Calendar.module.css";
+import { weekdays } from "../../../constants/constants.ts";
 
 export const Calendar = (): ReactElement => {
   return (
-    <div className={classes.container}>
-      <p>Calendar goes here</p>
+    <div className={classes.calendar}>
+      {weekdays.map((weekday) => (
+        <CalendarDay weekday={weekday} />
+      ))}
+    </div>
+  );
+};
+
+const CalendarDay = ({ weekday }): ReactElement => {
+  return (
+    <div className={classes.calendarDay}>
+      <h3 className={classes.dayHeading}>{weekday}</h3>
     </div>
   );
 };
