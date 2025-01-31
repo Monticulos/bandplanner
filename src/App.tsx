bbb70b5b868c0206401d629ec7a02de8c552ef76
Erling Hauan <erling.hauan@digdir.no>
2025-01-31 21:31:30 +0100

    rename Entry.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import "./index.css";
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { Header } from "./components/Header/Header.tsx";
 import { Footer } from "./components/Footer/Footer.tsx";
 import { MainContent } from "./components/MainContent/MainContent.tsx";
