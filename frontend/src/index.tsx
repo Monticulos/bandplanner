a5fc8624d2a26a3d2b58c3da32a1d686e68d2ee4
Monticulos <131489541+Monticulos@users.noreply.github.com>
2025-02-01 11:03:25 +0100

    rename components folder to app (#45)
    Co-authored-by: Erling Hauan <erling.hauan@digdir.no>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,9 @@
 import "./index.css";
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Header } from "./components/Header/Header.tsx";
-import { Footer } from "./components/Footer/Footer.tsx";
-import { MainContent } from "./components/MainContent/MainContent.tsx";
+import { Header } from "./app/Header/Header.tsx";
+import { Footer } from "./app/Footer/Footer.tsx";
+import { MainContent } from "./app/MainContent/MainContent.tsx";
 
 createRoot(document.body).render(
   <StrictMode>
