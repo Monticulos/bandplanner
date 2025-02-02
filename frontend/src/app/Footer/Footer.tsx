5f3b46cf7554b775ec554ceaed52c895b16a033e
Erling Hauan <erling.hauan@digdir.no>
2025-02-02 07:46:05 +0100

    add icons

--- frontend/src/app/Footer/Footer.tsx
+++ frontend/src/app/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 ﻿import React from "react";
 import classes from "./Footer.module.css";
+import { CodeIcon } from "../../icons/CodeIcon";
 
 export const Footer = () => {
   return (
     <footer className={classes.footer}>
       <a href="https://github.com/Monticulos/bandplanner" target="_blank">
+        <CodeIcon />
         Band practice planner
       </a>
     </footer>
