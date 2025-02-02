52d7988c0b803ee89d450a2ba289b886182252e6
Erling Hauan <erling.hauan@digdir.no>
2025-02-02 08:33:35 +0100

    button component and add new button

---
+++ frontend/src/icons/PlusIcon.tsx
@@ -0,0 +1,18 @@
+﻿import React from "react";
+
+export const PlusIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="3 -3 24 24"
+    focusable="false"
+    role="img"
+  >
+    <path
+      fill="currentColor"
+      d="M12.75 5.5a.75.75 0 0 0-1.5 0v5.75H5.5a.75.75 0 0 0 0 1.5h5.75v5.75a.75.75 0 0 0 1.5 0v-5.75h5.75a.75.75 0 0 0 0-1.5h-5.75z"
+    ></path>
+  </svg>
+);
