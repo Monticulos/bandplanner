aa8885bfc0553ed0205f6cfcf8338c236ede8780
Monticulos <131489541+Monticulos@users.noreply.github.com>
2025-02-02 07:49:50 +0100

    Add icons (#50)
    * add icons
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Erling Hauan <erling.hauan@digdir.no>

--- frontend/src/app/Header/Header.tsx
+++ frontend/src/app/Header/Header.tsx
@@ -1,10 +1,14 @@
 ﻿import React from "react";
 import classes from "./Header.module.css";
+import { CalendarIcon } from "../../icons/CalendarIcon";
 
 export const Header = () => {
   return (
     <header className={classes.header}>
-      <h1>Band practice planner</h1>
+      <h1>
+        <CalendarIcon />
+        Band practice planner
+      </h1>
     </header>
   );
 };
