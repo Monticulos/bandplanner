78ac15ddb1d7bac5578c9589bfb38c969f9272ee
Erling Hauan <erling.hauan@digdir.no>
2025-02-12 19:32:07 +0100

    create App.tsx

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,14 @@
+﻿import { Header } from "./Header/Header";
+import { MainContent } from "./MainContent/MainContent";
+import { Footer } from "./Footer/Footer";
+import React, { ReactElement } from "react";
+
+export function App(): ReactElement {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
