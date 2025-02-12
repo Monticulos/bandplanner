bf8effa07252cc09fa31e848b3bbb0d09ed1cf3e
Erling Hauan <erling.hauan@digdir.no>
2025-02-12 19:38:51 +0100

    move Calendar and ParticipantList into App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,12 +2,18 @@
 import { MainContent } from "./MainContent/MainContent";
 import { Footer } from "./Footer/Footer";
 import React, { ReactElement } from "react";
+import { ParticipantList } from "./MainContent/ParticipantList/ParticipantList";
+import classes from "./App.module.css";
+import { Calendar } from "./MainContent/Calendar/Calendar";
 
 export function App(): ReactElement {
   return (
     <>
       <Header />
-      <MainContent />
+      <div className={classes.middle}>
+        <Calendar />
+        <ParticipantList />
+      </div>
       <Footer />
     </>
   );
